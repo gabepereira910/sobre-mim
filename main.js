181508d5d63c9de581f04c4ded269c938374deb1
Gabriel Pereira <140936551+gabepereira910@users.noreply.github.com>
2024-06-28 17:03:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function mostrarMensagemLinke() {
+    alert("Seja bem vindo(a) ao meu perfil do Linkedin. Fique a vontade para enviar conexão! :)");
+    return true;
+}
+
+
+function mostrarMensagemInsta() {
+    alert("Seja bem vindo(a) ao meu perfil do Instagram!!");
+    return true;
+}
